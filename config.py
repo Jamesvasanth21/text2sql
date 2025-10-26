e6d4c9d1a677166d28b054e136c04831b40cc5dd
James Vasanth <jamesvasanth789@gmail.com>
2025-10-26 16:19:47 +0000

    Third Commit

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-FORECASTING_API_KEY = 'nixak-b657XtF5LBH2vBqGDnPHAuvunsJZE2HELPFVtl0n98v3siI9b72yDmWVRkTuVbD1T5YqpH9pXndvh0vf'
-API_KEY = "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"
+FORECASTING_API_KEY = ''
+API_KEY = ""
 API_BASE = "https://api.groq.com/openai/v1"
